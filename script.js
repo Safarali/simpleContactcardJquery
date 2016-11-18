dc7e06ee7cbe86f4ec1ddd95144f499624ab5bc5
Safarali <mr.safarali@gmail.com>
2016-11-17 16:55:13 -0800

    Simple contact card using jQuery created

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+
+
 $(document).ready(function() {
     $('.registration').submit(function () {
         var name = $('#name').val();
         var last = $('#last_name').val();
         var email = $('#email').val();
         var tel = $('#tel').val();
+        var desc = $('#description').val();
 
+        if (name && last && email && tel && desc) {
+            $('.details').append(`
+                <div class="card">
+                    <h4>${name} ${last}</h4>
+                    <p class="email">Email: ${email}</p>
+                    <p class="tel">Phone $: ${tel}</p>
+                    <button id="toggle-btn">Description</button>
+                    <button id="delete">Delete</button>
+                    <p class="description">About: ${desc}</p>
+                </div>`);
 
-        var newData = "<tr><td>"+name+"</td><td>"+last+"</td><td>"+email+"</td><td>"+tel+"</td></tr>";
-        $('tbody').append(newData);
-        $('#name').val("");
-        $('#last_name').val("");
-        $('#email').val("");
-        $('#tel').val("")
+                $('#name').val("");
+                $('#last_name').val("");
+                $('#email').val("");
+                $('#tel').val("");
+                $('#description').val("");
+                $('.description').hide();
+
+        }
         return false;
     });
-
+    $('.details').on('click', '#toggle-btn', function(){
+        $(this).siblings('.description').slideToggle(200)
+    });
+    $('.details').on('click', '#delete', function () {
+        $(this).parent().hide();
+    })
 });
